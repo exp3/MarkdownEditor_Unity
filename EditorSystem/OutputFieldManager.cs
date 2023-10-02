fe2b84b9c054724b7800876ba6c9102cc1bc747e
exp3 <45595203+exp3@users.noreply.github.com>
2023-10-03 00:33:33 +0900

    Update OutputFieldManager.cs

--- EditorSystem/OutputFieldManager.cs
+++ EditorSystem/OutputFieldManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class OutputFieldManager : MonoBehaviour
 {
+    [SerializeField] private GameObject previewObject;
+    private TMP_Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = previewObject.GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetPreview(string inputText)
+    {
+        string previewText = "";
+
+        ChangePreview(previewText);
+    }
+
+    public void ChangePreview(string previewText)
+    {
+        Debug.Log("changePreview");
+        this.text.SetText(previewText);
     }
 }
