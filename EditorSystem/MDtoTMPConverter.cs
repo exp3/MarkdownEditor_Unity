0a7ba8ebbbe8e969cabcb0a36cf2221899bd0b65
exp3 <45595203+exp3@users.noreply.github.com>
2023-10-03 01:22:45 +0900

    Update MDtoTMPConverter.cs

--- EditorSystem/MDtoTMPConverter.cs
+++ EditorSystem/MDtoTMPConverter.cs
@@ -1,14 +1,13 @@
 /// preview側の空オブジェクトにアタッチするクラス
 /// 文字列の変換のみを行うためMonoBehaviourは継承しない。
 
-using System.Collections;
-using System.Collections.Generic;
+
 using Markdig;
 
-public class MDtoTMPConverter
+public static class MDtoTMPConverter
 {
-    private Markdig.MarkdownPipeline markdownPipeline = new MarkdownPipelineBuilder().UsePipeTables().Build();
-    public string Convert(string text)
+    private static Markdig.MarkdownPipeline markdownPipeline = new MarkdownPipelineBuilder().UsePipeTables().Build();
+    public static string Convert(string text)
     {
         text = Markdown.ToHtml(text);
         return text;
